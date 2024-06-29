32d25560b6d9cbd3584b50e70b579d315433ee59
Vikram Aditya <vikkiranger@gmail.com>
2024-06-30 04:14:03 +0530

    initial commit

---
+++ student/src/test/java/online/vikramaaditya/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package online.vikramaaditya.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
